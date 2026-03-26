0ed77f2917a7ee24e65234e668770f813a2ab13f
Azienflv <121687950+Azienflv@users.noreply.github.com>
2026-03-26 18:59:27 -0400

    Update app.js

--- app.js
+++ app.js
@@ -47,19 +47,6 @@ function logout() {
 }
 
 // =======================
-// 🚀 menu deplegable
-// =======================
-
-function toggleMenu() {
-  const menu = document.getElementById("menu");
-
-  if (menu.style.display === "block") {
-    menu.style.display = "none";
-  } else {
-    menu.style.display = "block";
-  }
-}
-// =======================
 // 🚀 INIT
 // =======================
 document.addEventListener("DOMContentLoaded", () => {
