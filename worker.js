74182de9f1ea44697771ade19523c6c815149578
Azienflv <121687950+Azienflv@users.noreply.github.com>
2026-03-29 22:42:49 -0400

    Update CACHE_NAME to version v6

--- worker.js
+++ worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pcg-tours-v5"; // 🔥 cambia versión cada vez que actualices
+const CACHE_NAME = "pcg-tours-v6"; // 🔥 cambia versión cada vez que actualices
 
 const ASSETS = [
   "./",
